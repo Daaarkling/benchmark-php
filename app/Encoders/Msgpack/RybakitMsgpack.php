85a2da310bf30316006712c56d9d1a35e5c8bc8c
Eugene Leonovich <eugene.leonovich@sanoma.com>
2016-11-17 14:37:30 +0100

    Tweak pure msgpack benchmarks

--- app/Encoders/Msgpack/RybakitMsgpack.php
+++ app/Encoders/Msgpack/RybakitMsgpack.php
@@ -5,20 +5,19 @@ namespace Darkling\Benchmark\Encoders\Xml;
 
 use Darkling\Benchmark\IUnitBenchmark;
 use MessagePack\Packer;
-use MessagePack\Unpacker;
 
 
 class RybakitMsgpack implements IUnitBenchmark
 {
+    private $packer;
 
+    public function __construct()
+    {
+        $this->packer = new Packer(Packer::FORCE_STR | Packer::FORCE_MAP);
+    }
 
-	public function execute($data)
-	{
-		$packer = new Packer();
-		$packer->setTypeDetectionMode(Packer::FORCE_STR | Packer::FORCE_MAP);
-		$packed = $packer->pack($data);
-		return $packed;
-	}
-
-
+    public function execute($data)
+    {
+        return $this->packer->pack($data);
+    }
 }
